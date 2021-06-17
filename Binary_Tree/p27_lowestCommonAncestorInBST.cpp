3cc7969d7f243bfa1bcf0d9fec3326ddb7c3c3a3
ayanisation <ayanizationreaction@gmail.com>
2021-06-18 00:09:44 +0530

    adding questions

---
+++ Binary_Tree/p27_lowestCommonAncestorInBST.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void path(Node* r,int n,vector<Node*>&v){
+    if(!r)
+    return;
+    v.push_back(r);
+    if(n > r->data)
+    path(r->right,n,v);
+    else if(n < r->data)
+    path(r->left,n,v);
+    else
+    return;
+}
+Node* LCA(Node *root, int n1, int n2)
+{
+   vector<Node*>p1,p2;
+   path(root,n1,p1);
+   path(root,n2,p2);
+   p1.push_back(NULL);
+   p2.push_back(NULL);
+   for(int i=0;i<min(p1.size(),p2.size());i++){
+       if(p1[i]!=p2[i])
+       return p1[i-1];
+   }
+   return NULL;
+}
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        int l , h;
+		getline(cin,s);
+		scanf("%d ",&l);
+		scanf("%d ",&h);
+        Node* root = buildTree(s);
+        cout<<LCA(root ,  l ,  h)->data<<endl;
+    }
+    return 1;
+}
